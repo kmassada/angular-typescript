299b1ceca41ee72c7896c300a0e0ea99a106d3d4
Kenneth Massada <kmassada@videologygroup.com>
2016-02-23 07:40:41 -0500

    cleanup about venue

--- app/common/services/venueService.ts
+++ app/common/services/venueService.ts
@@ -2,18 +2,17 @@ module app.common {
     interface IVenueService{
 
     }
-    interface IVenueResource extends ng.resource.IResource<app.domain.IVenue>{
+    export interface IVenueResource extends ng.resource.IResource<app.domain.IVenue>{
 
     }
 
     export class VenueService implements IVenueService{
       static $inject = ["$resource", "url"];
       constructor(private $resource: ng.resource.IResourceService, private url: URL_CONSTANTS){
-        alert(this.url.BASE);
       }
 
     getVenueResource(): ng.resource.IResourceClass<IVenueResource>{
-      return this.$resource("/api/venues/:venueId");
+      return this.$resource(this.url.BASE_API+"/v1/venues/:id");
     }
   }
   angular
--- app/models/venue.ts
+++ app/models/venue.ts
@@ -1,11 +1,13 @@
 module app.domain {
   export interface IVenue {
+    _id: string;
     name: string;
     location: string;
     category: string;
   }
   export class Venue implements IVenue {
     constructor(
+      public _id: string,
       public name: string,
       public location: string,
       public category: string
